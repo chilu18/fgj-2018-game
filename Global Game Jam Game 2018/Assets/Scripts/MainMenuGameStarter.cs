41eb9a62c611680da762626e95a71046df277873
Jesse Luoto <jesse.luoto@gmail.com>
2018-01-28 00:05:41 +0200

    Add main menu

---
+++ Global Game Jam Game 2018/Assets/Scripts/MainMenuGameStarter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuGameStarter : MonoBehaviour {
+
+	public GameObject selectUserMenu;
+	public GameObject pressAnyKeyMenu;
+	public MenuButton virologistSelected;
+	bool isOnSelectUserMenu = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isOnSelectUserMenu) {
+			CheckPressAnyArrowKey ();
+		} else {
+			CheckEnterOrSpacebarPressed ();
+		}
+	}
+
+	void CheckPressAnyArrowKey() {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) ||
+			Input.GetKeyDown (KeyCode.RightArrow) ||
+			Input.GetKeyDown (KeyCode.UpArrow) ||
+			Input.GetKeyDown (KeyCode.DownArrow)) {
+			isOnSelectUserMenu = true;
+			selectUserMenu.SetActive (true);
+			pressAnyKeyMenu.SetActive (false);
+		}
+	}
+
+	void CheckEnterOrSpacebarPressed() {
+		if (Input.GetKeyDown (KeyCode.Space) ||
+			Input.GetKeyDown (KeyCode.KeypadEnter)) {
+			if (virologistSelected.isDown) {
+				Game.instance.isPlayingPatientZero = false;
+				Game.instance.isPlayingVirologist = true;
+			} else {
+				Game.instance.isPlayingPatientZero = true;
+				Game.instance.isPlayingVirologist = false;
+			}
+			SceneManager.LoadScene("Game Scene", LoadSceneMode.Single);
+		}
+	}
+}
---
+++ Global Game Jam Game 2018/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+
+	public KeyCode positiveKey;
+	public KeyCode negativeKey;
+	public GameObject disableThis;
+	public Text greyThis;
+	public bool isDown = false;
+
+	void Start() {
+		if (isDown)
+			Enable ();
+		else
+			Disable ();
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (negativeKey)) {
+			Disable ();
+		} else if (Input.GetKeyDown (positiveKey)) {
+			Enable ();
+		}
+	}
+
+	void Disable() {
+		Color currentColor = greyThis.color;
+		currentColor.a = 0.5f;
+		greyThis.color = currentColor;
+
+		disableThis.SetActive (false);
+		isDown = false;
+	}
+
+	void Enable() {
+		Color currentColor = greyThis.color;
+		currentColor.a = 1.0f;
+		greyThis.color = currentColor;
+
+		disableThis.SetActive (true);
+		isDown = true;
+	}
+}
