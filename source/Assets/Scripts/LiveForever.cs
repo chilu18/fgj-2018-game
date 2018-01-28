f1accb953a79802a5cfe6343344a2dc97efd0685
Jesse Luoto <jesse.luoto@gmail.com>
2018-01-28 12:49:19 +0200

    Add BG music to scene

---
+++ source/Assets/Scripts/LiveForever.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiveForever : MonoBehaviour {
+
+	public string uniqid = "";
+	// Use this for initialization
+	void Start () {
+		foreach (LiveForever maybeMe in FindObjectsOfType<LiveForever> ()) {
+			if (uniqid == maybeMe.uniqid && maybeMe != this) {
+				Destroy (gameObject);
+			}
+		}
+		Object.DontDestroyOnLoad (gameObject);
+	}
+
+}
