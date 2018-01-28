25010c81c3026fd94a8b7d0e71cc14d7a3621cfc
Jesse Luoto <jesse.luoto@gmail.com>
2018-01-28 11:35:45 +0200

    Add game over texts

--- Global Game Jam Game 2018/Assets/Scripts/Game.cs
+++ Global Game Jam Game 2018/Assets/Scripts/Game.cs
@@ -10,11 +10,19 @@ public class Game {
 
 	public static void VirologistWin () {
 		Debug.Log ("VIROLOGIST WIN!");
-		Time.timeScale = 0;
+		GameObject.FindObjectOfType<GameOverText> ().ShowGameOverTexts (true);
+		Game.EndGame ();
 	}
 
 	public static void PatientZeroWin () {
 		Debug.Log ("PATIENT ZERO WIN!");
+		GameObject.FindObjectOfType<GameOverText> ().ShowGameOverTexts (false);
+		Game.EndGame ();
+	}
+
+	private static void EndGame() {
 		Time.timeScale = 0;
+		GameObject.FindObjectOfType<Virologist> ().enabled = false;
+		GameObject.FindObjectOfType<PatientZero> ().enabled = false;
 	}
 }
---
+++ Global Game Jam Game 2018/Assets/Scripts/GameOverText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverText : MonoBehaviour {
+
+	public GameObject virologistWonText;
+	public GameObject patientZerotWonText;
+	public GameObject[] otherObjectsToShow;
+	private bool gameHasEnded = false;
+
+	public void ShowGameOverTexts(bool virologistWon) {
+		gameHasEnded = true;
+		if (virologistWon) {
+			virologistWonText.SetActive (true);
+		} else {
+			patientZerotWonText.SetActive (true);
+		}
+
+		foreach (GameObject go in otherObjectsToShow) {
+			go.SetActive (true);
+		}
+	}
+
+	void Update() {
+		if (!gameHasEnded)
+			return;
+
+		if (Input.GetKeyDown (KeyCode.Space) ||
+			Input.GetKeyDown (KeyCode.KeypadEnter)) {
+			SceneManager.LoadScene ("Menu", LoadSceneMode.Single);
+			Time.timeScale = 1;
+		}
+	}
+}
